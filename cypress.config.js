4dc95e6e6790938b1322b4d309ca98b435e058b7
jorgeDeLeon <devdeleonjorge@gmail.com>
2025-12-16 08:20:32 -0600

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+import 'dotenv/config'
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: process.env.URL
+  },
+  
+});
---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,11 @@
+describe('pruebiats app remota', () => {
+    it('web main online', () => {
+        cy.visit('/');
+    });
+
+    it('Should be visible contente main', () => {
+        cy.visit('/');
+        cy.contains('Vite + React'); 
+    });
+
+});
