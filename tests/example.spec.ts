cbac98d3f5083f9ccbc87326a4327052e61d2cc4
jorgeDeLeon <jdeleon@acsyt.com>
2026-03-09 23:51:51 -0600

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('http://localhost');
   await expect(page).toHaveTitle('Sensortrol');
 });
 
 test('Increment counter with button', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('http://localhost');
   const botonContador = page.getByRole('button', { name: /count is 0/i });
   await expect(botonContador).toBeVisible();
   await botonContador.click();
