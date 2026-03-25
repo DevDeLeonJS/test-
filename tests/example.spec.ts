ccc67e712af74f1ead8c37de6466959c8b2b89ba
DevDeLeonJS <132846023+DevDeLeonJS@users.noreply.github.com>
2026-03-25 12:43:43 -0600

    Update localhost:3000

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost');
+  await page.goto('http://localhost:3000');
   const text = await page.getByText('Sensortrol').innerText();
   expect(text).toBe('Sensortrol');
   expect(page).toHaveTitle('Sensortrol');
 });
 
 test('Increment counter with button', async ({ page }) => {
-  await page.goto('http://localhost');
+  await page.goto('http://localhost:3000');
   const button = page.getByRole('button', { name: /count is/ });
   await button.click();
   expect(await button.innerText()).toContain('1');
